ddb058314a6091e16d7cc1ef2708f17a259f4066
Vinicius Dias <carlosv775@gmail.com>
2022-02-28 16:18:18 -0300

    Video 2.2

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
-class SeriesController
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
 {
-    public function listarSeries()
+    public function index()
     {
         $series = [
             'Punisher',
