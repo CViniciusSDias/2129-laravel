0ee757320dc1a8a1744358e17db6d8402dba55db
Vinicius Dias <carlosv775@gmail.com>
2022-02-28 19:38:11 -0300

    Video 4.3

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,8 +9,10 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
-<h1>{{ $title }}</h1>
+<div class="container">
+    <h1>{{ $title }}</h1>
 
-{{ $slot }}
+    {{ $slot }}
+</div>
 </body>
 </html>
