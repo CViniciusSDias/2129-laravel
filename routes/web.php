26e62cbf9113ca88e70d5528488ec8d6b9b6bf22
Vinicius Dias <carlosv775@gmail.com>
2022-02-28 16:09:11 -0300

    Video 2.1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SeriesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/ola', function () {
-    echo 'Olá, Mundo!';
-});
+Route::get('/series', [SeriesController::class, 'listarSeries']);
